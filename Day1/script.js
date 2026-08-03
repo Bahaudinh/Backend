c211a4f803a7092678cb2f8e345588aa71f072ab
Bahauddin <bahauddin1232@gmail.com>
2026-08-03 23:04:47 +0500

    updated script.js file

--- Day1/script.js
+++ Day1/script.js
@@ -1,5 +1,13 @@
-let n=5;
-for(let i=0;i<n;i++){
-    console.log("Hello World");
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log("Hello World");
+// }
+// console.log("Good Bye!");
+
+// console.log(process.argv)
+
+let arguments=process.argv;
+
+for(let i=2;i<arguments.length;i++){
+    console.log("Hello "+arguments[i]);
 }
-console.log("Good Bye!");
