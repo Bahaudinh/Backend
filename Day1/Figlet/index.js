1a58ccd034e137e140b29a2d0790574974421d47
Bahauddin <bahauddin1232@gmail.com>
2026-08-04 13:41:09 +0500

    Implemented NPM Package and implemented figlet library (package)

---
+++ Day1/Figlet/index.js
@@ -0,0 +1,10 @@
+import figlet from "figlet";
+
+// async function doStuff() {
+//   const text = await figlet.text("Hello World!!");
+//   console.log(text);
+// }
+
+// doStuff();
+
+console.log(await figlet.text("B A H A U D D I N"));
