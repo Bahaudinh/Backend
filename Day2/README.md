75baa51b90445243df1fc7485024e42df59f8c39
Bahauddin <bahauddin1232@gmail.com>
2026-08-06 11:12:04 +0500

    learned about path parameters and search query

--- Day2/README.md
+++ Day2/README.md
@@ -7,3 +7,11 @@ and handle a basics of response in which response
 answer is provided in console means now if someone 
 send request on localhost port 3000 response will 
 be provided
+
+
+Learned about path parameters how to create a parameters 
+of path and also learned how to store it in variable
+let {q}=req.params 
+and also learned about search how to fae search query 
+and how to response https://localhost:8080/search?q=findsomething
+and extract a query by using let {q}=req.query
--- Day2/index.js
+++ Day2/index.js
@@ -1,14 +1,56 @@
-const express=require("express")
-const app=express()
+const express = require("express")
+const app = express()
 
-let port=8080
+let port = 8080
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is listening at port ${port}`);
 })
 
-app.use((req,res)=>{
-    console.log("Request Recieved");
-    code="<h1>Hello World</h1><ul><li>Nodejs</li><li>Express</li><li>MongoDB</li></ul>"
-    res.send(code);
+app.get("/", (req, res) => {
+    console.log("Root Path");
+    res.send("Welcome to the root path!");
 })
+
+app.post("/", (req, res) => {
+    res.send("Welcome to the root path by post request");
+})
+
+app.get("/apple", (req, res) => {
+    console.log("Apple Path");
+    res.send("Welcome to the apple path!");
+})
+
+app.get("/orange", (req, res) => {
+    console.log("Orange Path");
+    res.send("Welcome to the orange path!");
+})
+
+
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    res.send(`Search Results for query : ${q}`);
+})
+
+app.get("/:username", (req, res) => {
+    const { username } = req.params;
+    res.send(`Welcome ${username}`);
+})
+
+app.use((req, res) => {
+    res.status(404).send("Page not found");
+});
+
+
+
+
+
+
+
+
+
+// app.use((req,res)=>{
+//     console.log("Request Recieved");
+//     code="<h1>Hello World</h1><ul><li>Nodejs</li><li>Express</li><li>MongoDB</li></ul>"
+//     res.send(code);
+// })
