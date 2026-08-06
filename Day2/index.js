81ed87f02d4602f4a20d2b43efe3c4fcb68c0f6f
Bahauddin <bahauddin1232@gmail.com>
2026-08-06 23:28:54 +0500

    Again repractice of Express learned material

--- Day2/index.js
+++ Day2/index.js
@@ -1,45 +1,104 @@
-const express = require("express")
-const app = express()
+// const express = require("express")
+// const app = express()
 
-let port = 8080
+// let port = 8080
 
-app.listen(port, () => {
+// app.listen(port, () => {
+//     console.log(`Server is listening at port ${port}`);
+// })
+
+// app.get("/", (req, res) => {
+//     console.log("Root Path");
+//     res.send("Welcome to the root path!");
+// })
+
+// app.post("/", (req, res) => {
+//     res.send("Welcome to the root path by post request");
+// })
+
+// app.get("/apple", (req, res) => {
+//     console.log("Apple Path");
+//     res.send("Welcome to the apple path!");
+// })
+
+// app.get("/orange", (req, res) => {
+//     console.log("Orange Path");
+//     res.send("Welcome to the orange path!");
+// })
+
+
+// app.get("/search", (req, res) => {
+//     let { q } = req.query;
+//     res.send(`Search Results for query : ${q}`);
+// })
+
+// app.get("/:username", (req, res) => {
+//     const { username } = req.params;
+//     res.send(`Welcome ${username}`);
+// })
+
+// app.use((req, res) => {
+//     res.status(404).send("Page not found");
+// });
+
+
+
+
+
+
+
+const express=require("express");
+const app=express();
+
+let port=8080;
+
+app.listen(port,()=>{
     console.log(`Server is listening at port ${port}`);
 })
 
-app.get("/", (req, res) => {
+app.get("/",(req,res)=>{
     console.log("Root Path");
     res.send("Welcome to the root path!");
 })
 
-app.post("/", (req, res) => {
-    res.send("Welcome to the root path by post request");
+app.get("/help",(req,res)=>{
+    res.send("Welcome to the help page!");
 })
 
-app.get("/apple", (req, res) => {
-    console.log("Apple Path");
-    res.send("Welcome to the apple path!");
+app.get("/about",(req,res)=>{
+    res.send("Welcome to the about page!");
 })
 
-app.get("/orange", (req, res) => {
-    console.log("Orange Path");
-    res.send("Welcome to the orange path!");
+app.get("/contact",(req,res)=>{
+    res.send("Welcome to the contact page!");
 })
 
-
-app.get("/search", (req, res) => {
-    let { q } = req.query;
-    res.send(`Search Results for query : ${q}`);
+app.get("/search",(req,res)=>{
+    let {q}=req.query;
+    res.send(`Rsult for search : ${q}`);
 })
 
-app.get("/:username", (req, res) => {
-    const { username } = req.params;
+app.get("/:username",(req,res)=>{
+    let {username}=req.params;
     res.send(`Welcome ${username}`);
 })
 
-app.use((req, res) => {
+app.use((req,res)=>{
     res.status(404).send("Page not found");
-});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
