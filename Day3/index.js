8e5e49921bb9712b7fc1a7103f5982c82b096cf6
Bahauddin <bahauddin1232@gmail.com>
2026-08-10 16:52:55 +0500

    Updated a code

--- Day3/index.js
+++ Day3/index.js
@@ -1,28 +1,21 @@
 const express = require('express');
-const app = express();
-const port = 8080;
+const app=express();
+const port=8080;
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+app.listen(port,()=>{
+    console.log(`Server is running at port ${port}`);
 })
 
-const path = require('path');
+const path=require("path");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname+"/views"));
 
-app.get('/', (req, res) => {
-    console.log("Location is " + __dirname);
-    res.render('home.ejs');
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
 })
 
-app.get('/about', (req, res) => {
-    res.send('This is about page');
-})
-
-app.get('/rolldice', (req, res) => {
-    // console.log(Math.floor(Math.random()*6)+1);
-    let diceValue = Math.floor(Math.random() * 6) + 1;
-    console.log("Dice value is " + diceValue);  
-    res.render('rolldice.ejs', { diceValue });
+app.get("/rolldice",(req,res)=>{
+    let diceValue=Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs",{diceValue});
 })
